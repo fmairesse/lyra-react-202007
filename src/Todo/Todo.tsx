f43d335666858e08232ab64c188aa60bb225f4d0
Fabien Mairesse <fabien.mairesse@docdoku.com>
2020-07-07 11:30:18 +0200

    Styles Todo

--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -1,12 +1,13 @@
 import React, { ReactElement } from 'react'
 import { TodoModel } from '../model'
+import './Todo.css'
 
 interface TodoProps {
 	todo: TodoModel
 }
 
 export function Todo(props: TodoProps): ReactElement {
-	return <div>
+	return <div className={props.todo.completed ? 'completed' : 'not-completed'}>
 		<span>{props.todo.completed ? '☑' : '☐'}</span>
 		{props.todo.title}
 	</div>
