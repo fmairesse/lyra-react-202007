aa39d35165e993b059056e6c7dff7de6a44bfa06
Fabien Mairesse <fabien.mairesse@docdoku.com>
2020-07-07 12:09:20 +0200

    Gestion état todo

--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -6,9 +6,33 @@ interface TodoProps {
 	todo: TodoModel
 }
 
-export function Todo(props: TodoProps): ReactElement {
-	return <div className={props.todo.completed ? 'completed' : 'not-completed'}>
-		<span>{props.todo.completed ? '☑' : '☐'}</span>
-		{props.todo.title}
-	</div>
+interface TodoState {
+	completed: boolean
 }
+
+export class Todo extends React.Component<TodoProps, TodoState> {
+	constructor(props: TodoProps) {
+		super(props)
+		this.state = {
+			completed: props.todo.completed
+		}
+		// this.setState({
+		// 	completed: true
+		// })
+	}
+
+	onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const completed = e.target.checked
+		this.setState({completed: completed})
+	}
+
+	render(): ReactElement {
+		const t = this.props.todo
+		return <div className={t.completed ? 'completed' : 'not-completed'}>
+			<input type="checkbox" checked={this.state.completed} onChange={this.onChange} />
+			{t.title}
+		</div>
+	}
+}
+// export function Todo(props: TodoProps): ReactElement {
+// }
